e00a3b02f5de9497725cd1a4df96e9a880cac471
James Little <james@ergo.kiwi>
2019-03-28 22:46:56 +1300

    Strings should use single quotes

--- test/kata04/data-munging.test.js
+++ test/kata04/data-munging.test.js
@@ -4,10 +4,10 @@ const {weather, football} = require('../../src/kata04/data-munging');
 
 test('weather test', () => {
     expect.assertions(1);
-    return expect(weather("filename")).resolves.toEqual(-1);
+    return expect(weather('filename')).resolves.toEqual(-1);
 });
 
 test('football test', () => {
     expect.assertions(1);
-    return expect(football("filename")).resolves.toEqual(-1);
+    return expect(football('filename')).resolves.toEqual(-1);
 });
--- test/kata06/anagrams.test.js
+++ test/kata06/anagrams.test.js
@@ -4,5 +4,5 @@ const systemUnderTest = require('../../src/kata06/anagrams');
 
 test('test', () => {
     expect.assertions(1);
-    return expect(systemUnderTest("test")).resolves.toEqual(-1);
+    return expect(systemUnderTest('test')).resolves.toEqual(-1);
 });
